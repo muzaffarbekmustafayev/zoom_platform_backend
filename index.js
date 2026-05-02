86725da1d48617f945acd0d62a45c45d3ce97ebf
Muzaffarbek Mustafayev <muzaffarbekmustafayev@gmail.com>
2026-05-02 15:11:52 +0500

    updated

--- index.js
+++ index.js
@@ -447,7 +447,7 @@ function handleUserLeaving(socket, roomID) {
     delete socketToRoom[socket.id];
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     
